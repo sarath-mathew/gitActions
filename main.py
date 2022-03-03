1cce8ea6c63739edc857b4f1c105e6b131af5365
sarath-mathew <54340960+sarath-mathew@users.noreply.github.com>
2022-03-03 20:56:10 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def hello(): # Basic funcation to test linter functionality
-    print("hi")
-
-  def bye():
-    print("bye") # corrected indentation
+def hello(): # Basic funcation to test linter functionality
+  print("hi")
+def bye():
+  print("bye") # corrected indentation
 
 print(hello())
